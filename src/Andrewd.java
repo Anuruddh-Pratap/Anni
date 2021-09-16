71e6f89433b0728231fe5591a10de76ca586381d
Anuruddh-Pratap <90863157+Anuruddh-Pratap@users.noreply.github.com>
2021-09-16 23:40:07 +0530

    First file created

---
+++ src/Andrewd.java
@@ -0,0 +1,8 @@
+public class Andrewd {
+    public static void main(String[] args) {
+        Person Andrew=new Person();
+        Andrew.age=18;
+        Andrew.gender='M';
+        Andrew.display();
+    }
+}
