76e053f32902f9654422041ef67154fd7a058e04
Anuruddh-Pratap <90863157+Anuruddh-Pratap@users.noreply.github.com>
2021-11-16 16:10:37 +0530

    16-11-21 LAB

---
+++ src/exc.java
@@ -0,0 +1,12 @@
+public class exc {
+    public static void main(String[] args) {
+        try
+        {
+            System.out.println(7/0);
+        }
+        catch(Exception obj)
+        {
+            System.out.println(obj);
+        }
+    }
+}
